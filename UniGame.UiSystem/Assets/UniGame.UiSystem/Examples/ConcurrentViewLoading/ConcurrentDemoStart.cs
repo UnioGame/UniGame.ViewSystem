1af41baa588ff362131aaa75ef53d7d97b52c6f5
MikhailovSergei <mikhailovs@taktikagames.ru>
2020-03-11 12:49:56 +0300

    - add concurrent demo loading example

--- UniGame.UiSystem/Assets/UniGame.UiSystem/Examples/ConcurrentViewLoading/ConcurrentDemoStart.cs
+++ UniGame.UiSystem/Assets/UniGame.UiSystem/Examples/ConcurrentViewLoading/ConcurrentDemoStart.cs
@@ -1,18 +1,37 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UiSystem.Assets.UniGame.UiSystem.Examples.ConcurrentViewLoading;
+using UniGreenModules.UniGame.UiSystem.Runtime;
 using UnityEngine;
 
 public class ConcurrentDemoStart : MonoBehaviour
 {
+    public GameViewSystemComponent viewSystem;
+    
     // Start is called before the first frame update
     void Start()
     {
+        //async load views as windows
+        viewSystem.CreateWindow<DemoViewConcurrent1>(new ViewModelBase());
+        viewSystem.CreateWindow<DemoViewConcurrent2>(new ViewModelBase());
+        viewSystem.CreateWindow<DemoViewConcurrent3>(new ViewModelBase());
+        viewSystem.CreateWindow<DemoViewConcurrent4>(new ViewModelBase());
+        viewSystem.CreateWindow<DemoViewConcurrent5>(new ViewModelBase());
+        viewSystem.CreateWindow<DemoViewConcurrent6>(new ViewModelBase());
+        viewSystem.CreateWindow<DemoViewConcurrent7>(new ViewModelBase());
+        viewSystem.CreateWindow<DemoViewConcurrent8>(new ViewModelBase());
+        viewSystem.CreateWindow<DemoViewConcurrent9>(new ViewModelBase());
         
+        //async load views as screens
+        viewSystem.CreateScreen<DemoViewConcurrent1>(new ViewModelBase());
+        viewSystem.CreateScreen<DemoViewConcurrent2>(new ViewModelBase());
+        viewSystem.CreateScreen<DemoViewConcurrent3>(new ViewModelBase());
+        viewSystem.CreateScreen<DemoViewConcurrent4>(new ViewModelBase());
+        viewSystem.CreateScreen<DemoViewConcurrent5>(new ViewModelBase());
+        viewSystem.CreateScreen<DemoViewConcurrent6>(new ViewModelBase());
+        viewSystem.CreateScreen<DemoViewConcurrent7>(new ViewModelBase());
+        viewSystem.CreateScreen<DemoViewConcurrent8>(new ViewModelBase());
+        viewSystem.CreateScreen<DemoViewConcurrent9>(new ViewModelBase());
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
