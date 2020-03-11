a71859dfa2c6955011a4bb89b9d5d1bfda38445e
MikhailovSergei <mikhailovs@taktikagames.ru>
2020-03-11 12:32:53 +0300

    - update view asset reference to disposablereference

--- UniGame.UiSystem/Assets/UniGame.UiSystem/Runtime/Settings/UiViewsSourceReference.cs
+++ UniGame.UiSystem/Assets/UniGame.UiSystem/Runtime/Settings/UiViewsSourceReference.cs
@@ -1,10 +1,11 @@
 namespace UniGreenModules.UniGame.UiSystem.Runtime.Settings
 {
     using System;
+    using AddressableTools.Runtime.AssetReferencies;
     using UnityEngine.AddressableAssets;
 
     [Serializable]
-    public class UiViewsSourceReference : AssetReferenceT<UiViewsSource>
+    public class UiViewsSourceReference : DisposableAssetReference<UiViewsSource>
     {
         public UiViewsSourceReference(string guid) : base(guid) { }
     }
