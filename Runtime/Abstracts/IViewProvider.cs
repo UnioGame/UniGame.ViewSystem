ad8e4c5cb908a7eb5455307924a318490aece5fc
Vorontcov Gleb <vorontcovg@taktikagames.ru>
2020-06-02 10:44:45 +0300

    added IViewProvider and ViewExtensions

---
+++ Runtime/Abstracts/IViewProvider.cs
@@ -0,0 +1,10 @@
+﻿namespace UniModules.UniGame.UISystem.Runtime.Abstracts
+{
+    using global::UniGame.UiSystem.Runtime.Abstracts;
+    using UniRx.Async;
+
+    public interface IViewProvider
+    {
+        UniTask<T> CreateView<T>(IViewModel viewModel) where T : class, IView;
+    }
+}
