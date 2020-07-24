b17afbcd364f60a667c34fdb0e869e74a39b5c8d
Glongooo <glongooo4@gmail.com>
2020-07-24 14:50:18 +0300

    HOGM-1486 перенёс расширение для viewFactory в Extensions

--- Runtime/Abstracts/IViewFactory.cs
+++ Runtime/Abstracts/IViewFactory.cs
@@ -10,13 +10,4 @@
     {
         UniTask<IView> Create(Type viewType, string skinTag = "", Transform parent = null, string viewName = null);
     }
-
-    public static class IViewFactoryExtension
-    {
-        public static async UniTask<T> Create<T>(this IViewFactory factory, string skinTag = "", Transform parent = null, string viewName = null) where T : Component, IView
-        {
-            var view = await factory.Create(typeof(T), skinTag, parent, viewName) as T;
-            return view;
-        }
-    }
 }
