a96c0907a4d67fc68b8a0af66f6380c45c2419b0
MikhailovSergei <mikhailovs@taktikagames.ru>
2021-07-09 18:52:51 +0300

    - fix type dependency

--- Editor/ViewEditor/ViewContextMapSettingsEditor.cs
+++ Editor/ViewEditor/ViewContextMapSettingsEditor.cs
@@ -5,9 +5,9 @@ namespace UniModules.UniGame.ViewSystem.Editor.UiEditor
     using UnityEditor;
     using UnityEngine;
     
-#if !ODIN_INSPECTOR
-    [CustomEditor(typeof(ViewModelProviderSettings),editorForChildClasses:true)]
-#endif
+//#if !ODIN_INSPECTOR
+//    [CustomEditor(typeof(ViewModelProviderSettings),editorForChildClasses:true)]
+//#endif
     public class ViewContextMapSettingsEditor : UnityEditor.Editor
     {
         private ViewsAssemblyBuilder builder = new ViewsAssemblyBuilder();
