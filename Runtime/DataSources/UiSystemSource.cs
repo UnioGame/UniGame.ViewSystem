7bd80c7277935285dfe936a6411ee8b1b29efa4a
mikhailovs <mikhailovs@taktikagames.ru>
2021-10-08 02:50:30 +0300

    - update gameview system create view cancellation

--- Runtime/DataSources/UiSystemSource.cs
+++ Runtime/DataSources/UiSystemSource.cs
@@ -1,6 +1,7 @@
 ﻿namespace UniModules.UniGame.UISystem.Runtime.DataSources
 {
     using System;
+    using System.Threading;
     using Abstract;
     using Context.Runtime.Abstract;
     using Core.Runtime.Interfaces;
@@ -8,29 +9,23 @@
     using global::UniGame.ModelViewsMap.Runtime.Settings;
     using global::UniGame.UiSystem.Runtime;
     using UniCore.Runtime.Rx.Extensions;
+    using UniGameFlow.GameFlow.Runtime.Services;
     using UniModules.UniGame.AddressableTools.Runtime.Extensions;
     
     using UnityEngine;
-    using Object = UnityEngine.Object;
 
     [Serializable]
     [CreateAssetMenu(menuName = "UniGame/ViewSystem/UiSystemSource", fileName = "UiSystemSource")]
-    public class UiSystemSource : AsyncContextDataSource
+    public class UiSystemSource : DataSourceAsset<IGameViewSystem>
     {
         [SerializeField] 
         private AssetReferenceUiSystem uiSystemSource;
 
-        private static IGameViewSystem viewSystemAsset;
-        private static GameObject viewSystemObject;
-        
-        public async UniTask<IGameViewSystem> LoadSystem()
+
+        protected override async UniTask<IGameViewSystem> CreateInternalAsync(IContext context)
         {
-            if (viewSystemAsset != null && viewSystemObject!=null) {
-                return viewSystemAsset;
-            }
-            
             var uiSystem = await uiSystemSource.LoadGameObjectAssetTaskAsync(LifeTime);
-            var uiAsset = Instantiate(uiSystem);
+            var uiAsset  = Instantiate(uiSystem.gameObject).GetComponent<GameViewSystemAsset>();
             
             DontDestroyOnLoad(uiAsset.gameObject);
 
@@ -38,21 +33,10 @@
             {
                 if (!uiAsset)
                     return;
-                
-                viewSystemAsset.Cancel();
                 Destroy(uiAsset.gameObject);
-                viewSystemAsset = null;
             });
-                        
-            viewSystemAsset = uiAsset.ViewSystem;
-            return viewSystemAsset;
-        }
-        
-        public sealed override async UniTask<IContext> RegisterAsync(IContext context)
-        {
-            var uiSystem = await LoadSystem().AttachExternalCancellation(LifeTime.TokenSource);
-            context.Publish(uiSystem);
-            return context;
+
+            return uiAsset;
         }
     }
 }
