8377dba674976b6ce3d4840fb5f5453691c0e76c
Mefodei <mefodei.link@gmail.com>
2022-01-31 00:36:22 +0300

    update package version

--- Runtime/Extensions/ViewBindingExtension.cs
+++ Runtime/Extensions/ViewBindingExtension.cs
@@ -1,5 +1,5 @@
 ﻿using UniModules.UniGame.AddressableTools.Runtime.Extensions;
-using UniModules.UniGame.CoreModules.UniGame.AddressableTools.Runtime.Rx;
+using UniGame.Addressables.Reactive;
 
 namespace UniModules.Rx.Extensions
 {
