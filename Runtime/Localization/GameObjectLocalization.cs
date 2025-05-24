903de6c24884167cd48c95aff7ef3610916e3eec
Mefodei <mefodei.link@gmail.com>
2025-05-25 04:50:58 +0700

    GO localization

---
+++ Runtime/Localization/GameObjectLocalization.cs
@@ -0,0 +1,51 @@
+namespace UniGame.Localization.Runtime.Components
+{
+    using global::UniModules.UniCore.Runtime.DataFlow;
+    using UniModules.UniGame.Localization.Runtime;
+    using UniRx;
+    using UnityEngine;
+    using UnityEngine.Localization;
+
+    public class GameObjectLocalization : MonoBehaviour
+    {
+        public Transform parent;
+        public LocalizedAsset<GameObject> localization;
+     
+        private LifeTime _lifeTime = new();
+        private GameObject _instance;
+        private GameObject _asset;
+        
+        private void Awake()
+        {
+            parent ??= transform;
+        }
+
+        private void OnEnable()
+        {
+            _lifeTime.Restart();
+            
+            localization.AsObservable()
+                .Do(UpdateLocalization)
+                .Subscribe()
+                .AddTo(_lifeTime);
+        }
+
+        private void UpdateLocalization(GameObject asset)
+        {
+            if (asset == _asset) return;
+            
+            if(_instance!=null) Destroy(_instance);
+            
+            if (asset == null) return;
+            
+            _asset = asset;
+            _instance = Instantiate(_asset, parent);
+            _instance.DespawnWith(_lifeTime);
+        }
+
+        private void OnDisable()
+        {
+            _lifeTime.Restart();
+        }
+    }
+}
