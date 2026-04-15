d28d21a0ebe495982efcce37b03b0f8cc74d3985
Mefodei <mefodei.link@gmail.com>
2026-04-15 21:14:40 +0700

    - add auto register component

---
+++ Runtime/Utils/ViewAutoRegister.cs
@@ -0,0 +1,41 @@
+﻿namespace Utils
+{
+    using Cysharp.Threading.Tasks;
+    using UniGame.Context.Runtime;
+    using UniGame.UiSystem.Runtime;
+    using UniGame.ViewSystem.Runtime;
+    using UnityEngine;
+
+    public class ViewAutoRegister : MonoBehaviour
+    {
+        public ViewBase target;
+        public bool registerOnStart = true;
+        public bool registerOnEnable = false;
+        
+        public async UniTask RegisterView()
+        {
+            var context = await GameContext.GetContextAsync()
+                .AttachExternalCancellation(destroyCancellationToken);
+            
+            var viewSystem = await context
+                .ReceiveFirstAsync<IGameViewSystem>()
+                .AttachExternalCancellation(destroyCancellationToken);
+
+            var targetView = target ?? gameObject.GetComponent<IView>();
+            
+            await targetView.RegisterView(destroyCancellationToken);
+        }
+
+        private void Start()
+        {
+            if (registerOnStart)
+                RegisterView().Forget();
+        }
+
+        private void OnEnable()
+        {
+            if (registerOnEnable)
+                RegisterView().Forget();
+        }
+    }
+}
---
+++ Runtime/Utils/ViewInstancer.cs
@@ -0,0 +1,98 @@
+﻿namespace UniGame.UI.Common.Views
+{
+    using System.Collections.Generic;
+    using Context.Runtime;
+    using Cysharp.Threading.Tasks;
+    using global::Extensions;
+    using UniGame.Core.Runtime;
+    using UniGame.Core.Runtime.SerializableType;
+    using UniGame.UiSystem.Runtime.Settings;
+    using UniGame.ViewSystem.Runtime;
+    using UniModules.UniGame.UiSystem.Runtime;
+    using UnityEngine;
+    
+#if ODIN_INSPECTOR
+    using Sirenix.OdinInspector;
+#endif
+
+    public class ViewInstancer : MonoBehaviour
+    {
+#if ODIN_INSPECTOR
+        [ValueDropdown(nameof(GetViewsDropdown))]
+        [InlineButton(nameof(PingView))]
+#endif
+        public SType viewType;
+
+        public ViewType layout = ViewType.None;
+        public string viewTag = string.Empty;
+        public string viewName = string.Empty;
+        public bool stayWorldPosition = false;
+
+        public RectTransform container;
+        
+        public bool createOnStart = true;
+
+        public GameObject viewInstance;
+        
+        public IEnumerable<ValueDropdownItem<SType>> GetViewsDropdown()
+        {
+            foreach (var viewType in UiViewReference.GetTypeDropdown(typeof(IView)))
+            {
+                yield return viewType;
+            }
+        }
+        
+        public async UniTask CreateViewAsync()
+        {
+            var type = viewType.Type;
+            if (viewInstance || type == null) return;
+
+            var context = await GameContext.GetContextAsync()
+                .AttachExternalCancellation<IContext>(destroyCancellationToken);
+            
+            var viewSystem = await context
+                .ReceiveFirstAsync<IGameViewSystem>()
+                .AttachExternalCancellation<IGameViewSystem>(destroyCancellationToken);
+
+            IView view = null;
+            var lifeTime = this.GetAssetLifeTime();
+
+            switch (layout)
+            {
+                case ViewType.None:
+                    var viewParent = container == null ? transform : container;
+                    view = await viewSystem.Create(type,viewParent,viewTag,viewName,stayWorldPosition,lifeTime)
+                        .AttachExternalCancellation<IView>(destroyCancellationToken);
+                    view.GameObject.SetActive(true);
+                    break;
+                case ViewType.Screen:
+                    view = await viewSystem.OpenScreen(type,viewTag,viewName)
+                        .AttachExternalCancellation<IView>(destroyCancellationToken);
+                    break;
+                case ViewType.Window:
+                    view = await viewSystem.OpenWindow(type,viewTag,viewName)
+                        .AttachExternalCancellation<IView>(destroyCancellationToken);
+                    break;
+                case ViewType.Overlay:
+                    view = await viewSystem.OpenOverlay(type,viewTag,viewName)
+                        .AttachExternalCancellation<IView>(destroyCancellationToken);
+                    break;
+            }
+
+            viewInstance = view?.GameObject;
+        }
+
+        public void PingView()
+        {
+            var type = viewType.Type;
+            if (type == null) return;
+            type.PingView();
+        }
+        
+        private void Start()
+        {
+            if(createOnStart) 
+                CreateViewAsync().Forget();
+        }
+    }
+}
